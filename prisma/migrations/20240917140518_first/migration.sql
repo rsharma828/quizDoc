c1ad9def17390f3397c38af02e5b32a97e739af8
Rupesh sharma <rsharma@Rk-Air.local>
2024-09-17 19:39:51 +0530

    first upload

--- prisma/migrations/20240917140518_first/migration.sql
+++ prisma/migrations/20240917140518_first/migration.sql
@@ -20,11 +20,30 @@ CREATE TABLE "Pdf" (
     CONSTRAINT "Pdf_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdById" TEXT NOT NULL,
+    "chatId" TEXT NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_userId_key" ON "User"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Pdf_id_key" ON "Pdf"("id");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Message_id_key" ON "Message"("id");
+
 -- AddForeignKey
 ALTER TABLE "Pdf" ADD CONSTRAINT "Pdf_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Pdf"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
